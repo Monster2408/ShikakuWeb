94a64aaf38b5797e064d8f654ff96faa86759de7
Monster2408 <meoto2408@gmail.com>
2021-01-28 17:33:33 +0900

    フッターに©を追加

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -1,5 +1,5 @@
 <a href="" class="topBtn" id="topBtn">TOP</a><!--/固定ボタン-->
 <footer>
-    <p>footer</p>
+    <p>shikaku-net.xyz &copy; 2020-<?php echo date("Y"); ?></p>
 </footer>
 <script type="text/javascript" charset="utf-8" src="<?php echo $conf["url"]; ?>/assets/js/footer.js"></script>
